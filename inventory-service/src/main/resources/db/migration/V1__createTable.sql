6a178b1dd7187bb7e5fa8ed8d738cc188f726293
frizzyondabeat <frizzyondabeat@gmail.com>
2022-06-14 19:19:48 +0100

    Created Migrations on inventory_service and about to start Service Discovery

---
+++ inventory-service/src/main/resources/db/migration/V1__createTable.sql
@@ -0,0 +1,7 @@
+create table if not exists inventory (
+    id bigint not null auto_increment ,
+    sku_code varchar(100) not null ,
+    quantity integer not null ,
+    primary key (id) ,
+    unique key sku_code_unique(sku_code)
+)
---
+++ inventory-service/src/main/resources/db/migration/V2__addDataToTable.sql
@@ -0,0 +1,2 @@
+insert into inventory (sku_code, quantity) values ('iPhone13', 0);
+insert into inventory (sku_code, quantity) values ('iPhone13_blue', 78);
